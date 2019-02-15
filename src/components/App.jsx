8fa0610041408945be0b8a61cbc60ca22b2bbc23
Alex Garcia <g.nightster11@gmail.com>
2019-02-15 14:13:46 -0800

    add jsx files that were missing and added links to path ways to components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,8 @@ import Header from './Header';
 import Home from './Home';
 import WorkExperience from './workExperience/WorkExperience';
 import Project from './projects/Project';
+import Interests from './interests/Interests';
+import Education from './education/Education';
 
 
 function App(){
@@ -13,10 +15,10 @@ function App(){
       <Header/>
         <Switch>
           <Route exact path='/' component={Home} />
-          <Route exact path='/workExperience' component={WorkExperience} />
-          <Route exact path='/Project' component={Project} />
-          <Route exact path='/' component={Home} />
-          <Route exact path='/' component={Home} />
+          <Route path='/workExperience' component={WorkExperience} />
+          <Route path='/Project' component={Project} />
+          <Route path='/Interests' component={Interests} />
+          <Route path='/Education' component={Education} />
           <Route component={Error404} />
         </Switch>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 function Header(){
   return (
     <div>
       <h1>Hello</h1>
- 
+      <Link to="/">Home</Link> | <Link to="/workExperience">WorkExperience</Link> | <Link to="/Project">Project</Link> | <Link to="/Interests">Interests</Link> | <Link to="/Education">Education</Link>
     </div>
   );
 }
