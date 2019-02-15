28f26826f53b27268a501884613724d614f3d678
Alex Garcia <g.nightster11@gmail.com>
2019-02-15 15:45:17 -0800

    working on styling pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import {Button} from 'react-materialize';
-import { Link } from 'react-router-dom';
+import { Link, Icon } from 'react-router-dom';
 
 
 function Header(){
   
    const toolBar = {
-     margin : '50px'
+     margin : '50px',
+     border: '2px black solid',
+     padding: '20px'     
    }
 
   const bottonColor ={
-    color: 'white'
+    color: 'black',
   }
 
 
   return (
     <div style={toolBar} >
-    <Button waves='light' className='green'><Link to="/" style={bottonColor}>Home</Link></Button>
-    <Button waves='light' className='#ffbd00'><Link to="/workExperience" style={bottonColor}>WorkExperience</Link></Button>
-    <Button waves='light' className='#ffbd00'><Link to="/Project" style={bottonColor}>Project</Link></Button>
-    <Button waves='light' className='#ffbd00'><Link to="/Interests" style={bottonColor}>Interests</Link></Button>
-    <Button waves='light' className='#ffbd00'><Link to="/Education" style={bottonColor}>Education</Link></Button>
+    <Button waves='light' className='blue-grey lighten-4'><Link to="/" style={bottonColor}>Home</Link></Button>     
+    <Button waves='light' className='blue-grey lighten-4'><Link to="/workExperience" style={bottonColor}>WorkExperience</Link></Button>
+    <Button waves='light' className='blue-grey lighten-4'><Link to="/Project" style={bottonColor}>Project</Link></Button>
+    <Button waves='light' className='blue-grey lighten-4'><Link to="/Interests" style={bottonColor}>Interests</Link></Button>
+    <Button waves='light' className='blue-grey lighten-4'><Link to="/Education" style={bottonColor}>Education</Link></Button>
     </div>
   );
 }
