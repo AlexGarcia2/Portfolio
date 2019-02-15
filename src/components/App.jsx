f77fb3ebaeff1d56c065583890772b25a58dbefb
Alex Garcia <g.nightster11@gmail.com>
2019-02-15 13:52:30 -0800

    add imports and path ways to App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,8 @@ import { Switch, Route } from 'react-router-dom';
 import Error404 from './Error404';
 import Header from './Header';
 import Home from './Home';
+import WorkExperience from './workExperience/WorkExperience';
+import Project from './projects/Project';
 
 
 function App(){
@@ -10,6 +12,10 @@ function App(){
     <div>
       <Header/>
         <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/workExperience' component={WorkExperience} />
+          <Route exact path='/Project' component={Project} />
+          <Route exact path='/' component={Home} />
           <Route exact path='/' component={Home} />
           <Route component={Error404} />
         </Switch>
