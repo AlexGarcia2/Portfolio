d583bb4b9615566ab3e493e4c2ba2760d024aa15
Alex Garcia <g.nightster11@gmail.com>
2019-02-15 14:30:01 -0800

    add functions and links to components and compiled and links work

---
+++ src/components/interests/Interests.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Interests(){
+  return (
+    <div>
+      <h1>Interests go here</h1>
+    </div>
+  );
+}
+
+export default Interests;
---
+++ src/components/workExperience/WorkExperience.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function WorkExperience(){
+  return (
+    <div>
+      <h1>work experience here</h1>
+    </div>
+  );
+}
+
+export default WorkExperience;
