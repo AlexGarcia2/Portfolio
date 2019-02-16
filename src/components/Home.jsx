287715da3019b0f73d22cf1c97e6382ae8b04bf6
Alex Garcia <g.nightster11@gmail.com>
2019-02-15 16:35:57 -0800

    working on name badge in home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,19 @@ import  React  from 'react';
 
 function Home(){
 
+  const nameCard={
+    border: '2px white solid',
+    width: '400px',
+    textAlign: 'center',
+    fontFamily: 'impact',
+    positon: 'relitive',
+    right: '400px'
+  }
+
     return (
-      <div>
-        <p>home page here</p>
+      <div style={nameCard}>
+       <h2>ALex Garcia</h2>
+       <h3>Junior Developer</h3>
       </div>
     );
   }
