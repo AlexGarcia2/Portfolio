703b0793725934ce9161320eab5d894f9a395d1e
Alex Garcia <g.nightster11@gmail.com>
2019-02-22 14:41:39 -0800

    i have idea of the route i want to go but i need to get background image on template to work

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -4,17 +4,15 @@ import  React  from 'react';
 function HomeCard(){
 
   const nameCard={
-    // border: '2px white solid',
+    border: '2px white solid',
     width: '400px',
     textAlign: 'center',
     fontFamily: 'impact',
     marginTop: '100px',
     marginLeft:'450px',
-    // borderRadius: '10px/50px/30px',
-    boxShadow: 'inset 0 0 0 3000px',
-    filter: 'blur(10px)',
-    
-    background: 'rgba(211, 211, 211, 0.3)',
+    borderRadius: '10px/50px/30px',
+    boxShadow: ' 5px 5px black',   
+    background: 'rgba(211, 211, 211, 0.2)',
     
     
   }
@@ -23,19 +21,12 @@ function HomeCard(){
   }
 
     return (     
-      <div style={nameCard}>
-        <div  >
-          <div style={cardContent}>
+      <div>
+          <div style={nameCard}>
 
             <h4>Alex Garcia</h4>
-            <h5>Developer</h5>         
+            <h5>Junior Developer</h5>         
           </div>
-        </div>
-        <div>
-
-        </div>
-
-
       </div>     
     );
   }
