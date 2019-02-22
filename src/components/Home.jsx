a3ed9a6ecca707e4b3af82c572bb4bdfc623fde8
Alex Garcia <g.nightster11@gmail.com>
2019-02-22 11:13:45 -0800

    working on home page tryig to div to over lap pic

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,15 @@
 import  React  from 'react';
+import HomeCard from './HomeCard';
+import VWBack from '../assets/VWBack.jpg';
 
 function Home(){
 
-  const nameCard={
-    border: '2px white solid',
-    width: '400px',
-    textAlign: 'center',
-    fontFamily: 'impact',
-    marginTop: '100px',
-    marginLeft:'450px',
-    borderRadius: '10px/50px/30px'
-  }
+  
 
     return (
-      <div style={nameCard}>
-       <h4>Alex Garcia</h4>
-       <h5>Developer</h5>
+      <div>        
+        <HomeCard/>
+        <img src={VWBack}/>             
       </div>
     );
   }
---
+++ src/components/HomeCard.jsx
@@ -0,0 +1,25 @@
+import  React  from 'react';
+
+
+function HomeCard(){
+
+  const nameCard={
+    border: '2px white solid',
+    width: '400px',
+    textAlign: 'center',
+    fontFamily: 'impact',
+    marginTop: '100px',
+    marginLeft:'450px',
+    borderRadius: '10px/50px/30px'
+  }
+
+    return (          
+        <div style={nameCard}>
+            <h4>Alex Garcia</h4>
+            <h5>Developer</h5>         
+        </div>
+    );
+  }
+  
+  export default HomeCard;
+   
